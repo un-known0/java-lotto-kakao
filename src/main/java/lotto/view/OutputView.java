a9bfc791817c63c9eb8813c127bd0e5d7aa009c4
semi-colon-00 <semi.colon@kakaocorp.com>
2026-02-20 10:00:45 +0900

    chore: else구문 대신 return문 사용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -25,9 +25,9 @@ public class OutputView {
         System.out.printf("총 수익률은 %.2f입니다.", rate);
         if (rate < 1) {
             System.out.println("(기준이 1이기 때문에 결과적으로 손해라는 의미임)");
-        } else {
-            System.out.println();
+            return;
         }
+        System.out.println();
     }
 
 }
